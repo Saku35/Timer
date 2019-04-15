9ccc895a8486b17a28f07990e84a3ed95cbdc507
saku35 <>
2019-04-15 22:57:38 +0900

    #8 スペースだけひとまず気持ち悪くないように直してみました。

--- main/sketch.js
+++ main/sketch.js
@@ -11,42 +11,37 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
 
-  textAlign(LEFT,CENTER);
+  textAlign(LEFT, CENTER);
   textSize(40);
   frameRate(5);
   remaining_time = measuring_time;
 }
 
 function draw() {
-
-  if (IsStop === 0){
+  if (IsStop === 0) {
     var time_now = new Date();
     time_elapsed = time_now.getTime() - time_base;
     
     remaining_time = measuring_time - time_elapsed;
   }
 
-  if(remaining_time<=0){
+  if (remaining_time <= 0) {
     noLoop();
-  }
-  else{
+  } else {
     clear();
-    text(Math.floor(remaining_time/ 1000), 100, 100);
+    text(Math.floor(remaining_time / 1000), 100, 100);
   }
-  
 }
 
-function keyPressed(){
-  
-  if (keyCode === 32){
-    if (IsStop === 1){
+function keyPressed() {
+  if (keyCode === 32) {
+    if (IsStop === 1) {
       var tmp = new Date();
       time_base = tmp.getTime();
       IsStop = 0;
-    }
-    else{
+    } else {
       measuring_time = measuring_time - time_elapsed;
-      IsStop =1;
+      IsStop = 1;
     }
   }
 }
